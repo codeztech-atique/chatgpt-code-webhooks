35e3f93e90150c691fbc016000cfbdb1c3894543
Atique Ahmed <atique.ahmed@zoom.us>
2024-04-22 18:04:05 +0530

    fix big

--- controllers/resourceController.js
+++ controllers/resourceController.js
@@ -6,9 +6,11 @@ exports.fetchAndForwardCommitData = async(req, res) => {
         const analyzeCommits = await service.analyzeCommitChanges(req.body);
         res.status(config.get('success').statusCode).send(analyzeCommits);
     } catch(err) {
-        res.send(config.get('error').statusCode).send({
-            error: err
-        })
+        console.error("Error processing request:", err);
+        // Corrected to use status() correctly before sending the error response
+        res.status(config.get('error').statusCode).send({
+            error: err.message  // It's better to send err.message to avoid sending sensitive error data
+        });
     }
 }
 
